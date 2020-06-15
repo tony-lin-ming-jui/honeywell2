d4aa5e4f0a7c7e905b7ad620e925a0ecbb7d17ec
tony-lin-ming-jui <31836276+tony-lin-ming-jui@users.noreply.github.com>
2020-06-15 09:25:07 +0800

    Add files via upload

---
+++ honeywell2/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnAlarm = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.BtnZero = New System.Windows.Forms.Button()
+        Me.BtnMinus1 = New System.Windows.Forms.Button()
+        Me.BtnInstructions = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.BtnFind = New System.Windows.Forms.Button()
+        Me.textboxCOM = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(619, 53)
+        Me.BtnStart.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(140, 77)
+        Me.BtnStart.TabIndex = 0
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(73, 361)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 24)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "現在時間"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(194, 153)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(580, 78)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(73, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 24)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "COM Port"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(73, 164)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 24)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "條碼設定"
+        '
+        'BtnAlarm
+        '
+        Me.BtnAlarm.Location = New System.Drawing.Point(948, 53)
+        Me.BtnAlarm.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnAlarm.Name = "BtnAlarm"
+        Me.BtnAlarm.Size = New System.Drawing.Size(209, 77)
+        Me.BtnAlarm.TabIndex = 9
+        Me.BtnAlarm.Text = "解除錯誤警告"
+        Me.BtnAlarm.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(73, 450)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 24)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "產品條碼"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(73, 310)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 24)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "上次時間"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(944, 310)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(112, 24)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "產品數量:"
+        '
+        'BtnZero
+        '
+        Me.BtnZero.Location = New System.Drawing.Point(948, 376)
+        Me.BtnZero.Name = "BtnZero"
+        Me.BtnZero.Size = New System.Drawing.Size(137, 51)
+        Me.BtnZero.TabIndex = 13
+        Me.BtnZero.Text = "數量歸0"
+        Me.BtnZero.UseVisualStyleBackColor = True
+        '
+        'BtnMinus1
+        '
+        Me.BtnMinus1.Location = New System.Drawing.Point(948, 456)
+        Me.BtnMinus1.Name = "BtnMinus1"
+        Me.BtnMinus1.Size = New System.Drawing.Size(137, 56)
+        Me.BtnMinus1.TabIndex = 14
+        Me.BtnMinus1.Text = "數量減1"
+        Me.BtnMinus1.UseVisualStyleBackColor = True
+        '
+        'BtnInstructions
+        '
+        Me.BtnInstructions.Location = New System.Drawing.Point(1027, 600)
+        Me.BtnInstructions.Name = "BtnInstructions"
+        Me.BtnInstructions.Size = New System.Drawing.Size(209, 74)
+        Me.BtnInstructions.TabIndex = 15
+        Me.BtnInstructions.Text = "系統操作說明"
+        Me.BtnInstructions.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(50, 600)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(938, 96)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "如發生任何異常請點擊[解除錯誤警告]，方可繼續，如果還是無法恢復請重新開啟本程式" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "如果有問題需要協助請找 張老師(分機742) 或 明叡(分機743)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label8.Font = New System.Drawing.Font("新細明體", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label8.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.Label8.Location = New System.Drawing.Point(195, 422)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(579, 90)
+        Me.Label8.TabIndex = 39
+        '
+        'BtnFind
+        '
+        Me.BtnFind.Location = New System.Drawing.Point(323, 40)
+        Me.BtnFind.Name = "BtnFind"
+        Me.BtnFind.Size = New System.Drawing.Size(99, 49)
+        Me.BtnFind.TabIndex = 41
+        Me.BtnFind.Text = "尋找"
+        Me.BtnFind.UseVisualStyleBackColor = True
+        '
+        'textboxCOM
+        '
+        Me.textboxCOM.Location = New System.Drawing.Point(184, 50)
+        Me.textboxCOM.Name = "textboxCOM"
+        Me.textboxCOM.Size = New System.Drawing.Size(122, 35)
+        Me.textboxCOM.TabIndex = 42
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 23.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(1291, 699)
+        Me.Controls.Add(Me.textboxCOM)
+        Me.Controls.Add(Me.BtnFind)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.BtnInstructions)
+        Me.Controls.Add(Me.BtnMinus1)
+        Me.Controls.Add(Me.BtnZero)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.BtnAlarm)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Font = New System.Drawing.Font("新細明體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Xenon1900條碼閱讀機"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BtnAlarm As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents BtnZero As Button
+    Friend WithEvents BtnMinus1 As Button
+    Friend WithEvents BtnInstructions As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents BtnFind As Button
+    Friend WithEvents textboxCOM As TextBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ honeywell2/Form1.vb
@@ -0,0 +1,191 @@
+﻿Imports System.IO.Ports
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim strSerialRead As String
+    Dim blnRun As Boolean = True
+    Dim time As String
+    Dim cnt As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label6.Text = "產品數量: " & cnt
+        Label8.Text = ""
+        BtnAlarm.Enabled = False
+        TextBox2.Focus()
+    End Sub
+    Private Sub SerialPort1_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim sp As SerialPort = CType(sender, SerialPort)
+        Dim strTemp As String
+
+        strTemp = sp.ReadExisting() '讀到serialport的文字'
+        ''''''''strTemp = sp.ReadLine()
+        If blnRun Then 'and Button1.Text = "Stop"  //發生錯誤時用來關掉這裡讓條碼不讀取
+
+            '''''''strTemp = sp.ReadExisting()
+            strSerialRead = strTemp
+
+        End If
+    End Sub
+
+    Private Sub BtnFind_Click(sender As Object, e As EventArgs) Handles BtnFind.Click
+        Try
+            Dim wmi As Object = GetObject("winmgmts:\\.\root\cimv2")
+            Dim devices As Object = wmi.ExecQuery("Select DeviceID, Name from Win32_PnPEntity")
+            Dim strDeviceId As String
+            textboxCOM.Text = "N/A"
+            For Each d As Object In devices
+                strDeviceId = IIf(IsDBNull(d.DeviceID), "N/A", d.DeviceID)
+                If Strings.InStr(strDeviceId, "VID_0C2E&PID_090A") > 0 Then
+                    If Strings.InStr(d.Name, "(") > 0 Then
+                        Dim rm As MatchCollection
+                        rm = Regex.Matches(d.Name, "\b(COM\w+)\b")
+                        textboxCOM.Text = rm(0).Value
+                        Try
+                            SerialControl()
+                        Catch ex As Exception
+                            MsgBox("條碼槍COM Port 錯誤或未連接設備", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "錯誤")
+
+                            SerialPort1.Close()
+                            strSerialRead = ""
+
+                            Exit Sub
+                        End Try
+                        strSerialRead = ""
+                        Timer1.Enabled = True
+                    End If
+                End If
+            Next
+        Catch ex As Exception
+            'MsgBox(ex.ToString)
+            textboxCOM.Text = "N/A"
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If strSerialRead <> "" Then
+            TextBox2.Text = strSerialRead
+            strSerialRead = ""
+        End If
+    End Sub
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        Dim ask As Integer
+        If TextBox2.Text <> "" Then
+            Try
+                If BtnStart.Text = "Start" Then
+                    BtnStart.Text = "Stop"
+
+                    Timer1.Enabled = False
+
+                    TextBox2.Enabled = False
+                    textboxCOM.Enabled = False
+                    BtnFind.Enabled = False
+
+                    SerialControl()
+                    Timer2.Enabled = True
+                    strSerialRead = ""
+                    'TextBox1.Text = ""
+                    Label8.Text = ""
+                    Me.BackColor = Color.FromKnownColor(KnownColor.Control) '設定成沒有顏色'
+                Else
+                    ask = MsgBox("確定要停止?(數量計算將歸0重新計算)", MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "提醒")
+                    If ask = 1 Then
+                        BtStop()
+                        strSerialRead = ""
+                        Label8.Text = ""
+                        Me.BackColor = Color.FromKnownColor(KnownColor.Control)
+                        cnt = 0
+                        Label6.Text = "產品數量: " & cnt
+                        TextBox2.Focus()
+                        SerialControl()
+
+                    End If
+                End If
+            Catch ex As Exception
+                'MsgBox(ex.ToString, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "錯誤")  '可以改如果comport錯誤想要輸入的文字  有兩種情況 空白和COM錯誤 先別改這個除非能判斷'
+                MsgBox("COM Port 錯誤或未連接設備", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "錯誤")
+                SerialPort1.Close()
+                BtStop()
+            End Try
+        Else
+            MsgBox("請輸入條碼", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "警告")
+        End If
+
+    End Sub
+    Sub SerialControl()
+        SerialPort1.Close()
+        SerialPort1.PortName = textboxCOM.Text
+        SerialPort1.Open()
+        SerialPort1.DiscardInBuffer()
+        SerialPort1.DiscardOutBuffer()
+    End Sub
+    Sub BtStop()
+        BtnStart.Text = "Start"
+        TextBox2.Enabled = True
+        textboxCOM.Enabled = True
+        BtnFind.Enabled = True
+        Timer2.Enabled = False
+        Timer1.Enabled = True
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        If strSerialRead <> "" Then
+            If strSerialRead = TextBox2.Text Then
+                ReadBarCode(1)
+            Else
+                '錯誤'
+                Console.Beep(1000, 500)
+                ReadBarCode(0)
+            End If
+            strSerialRead = ""
+        End If
+    End Sub
+    Sub ReadBarCode(same As Integer)
+        Label5.Text = ""
+        Label5.Text = "上次條碼讀取時間: " & time
+        time = Now.ToString("yyyy-MM-dd HH:mm:ss.fff")
+        Label1.Text = "本次條碼讀取時間: " & time
+        Label8.Text = strSerialRead
+        strSerialRead = ""
+        If same = 1 Then
+            Me.BackColor = Color.FromArgb(0, 255, 0)
+            cnt = cnt + 1
+            Label6.Text = "產品數量: " & cnt
+        ElseIf same = 0 Then
+            Me.BackColor = Color.FromArgb(255, 0, 0)
+            BtnStart.Enabled = False
+            BtnAlarm.Enabled = True
+            BtnZero.Enabled = False
+            BtnMinus1.Enabled = False
+            blnRun = False
+            MsgBox("產品條碼與設定條碼不相同" + vbCrLf + "請點擊解除錯誤警告按鈕解除錯誤", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "錯誤")
+        End If
+    End Sub
+    Private Sub BtnAlarm_Click(sender As Object, e As EventArgs) Handles BtnAlarm.Click
+        blnRun = True
+        Me.BackColor = Color.FromKnownColor(KnownColor.Control)
+        strSerialRead = ""
+        'TextBox1.Text = ""
+        Label8.Text = ""
+        BtnStart.Enabled = True
+        BtnAlarm.Enabled = False
+        BtnZero.Enabled = True
+        BtnMinus1.Enabled = True
+    End Sub
+
+    Private Sub BtnZero_Click(sender As Object, e As EventArgs) Handles BtnZero.Click
+        cnt = 0
+        Label6.Text = "產品數量: " & cnt
+    End Sub
+
+    Private Sub BtnMinus1_Click(sender As Object, e As EventArgs) Handles BtnMinus1.Click
+        cnt = cnt - 1
+        If cnt < 0 Then
+            cnt = 0
+        End If
+        Label6.Text = "產品數量: " & cnt
+    End Sub
+
+    Private Sub BtnInstructions_Click(sender As Object, e As EventArgs) Handles BtnInstructions.Click
+        Form2.Show()
+    End Sub
+End Class
